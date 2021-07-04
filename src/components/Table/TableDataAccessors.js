dc60c1766ab3ad58e7cfe69994bf16386251ac08
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-07-04 20:59:24 +0500

    feat: добавляю accessors для доступа к свойствам данных таблицы

---
+++ src/components/Table/TableDataAccessors.js
@@ -0,0 +1,14 @@
+export const TableDataAccessors = [
+  'traffic.views',
+  'traffic.clicks',
+  'traffic.sessions',
+  'traffic.ctr',
+  'traffic.clickCost',
+  'traffic.expenses',
+  'sales.counter',
+  'sales.cpa',
+  'sales.proceeds',
+  'ga.counter',
+  'ga.cpa',
+  'ga.cr',
+];
