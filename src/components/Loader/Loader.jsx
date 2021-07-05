14959e03c4687386b45c6d4574b67f74b87495ed
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-07-06 03:26:13 +0500

    fix: убрал обертку лоадера для его позиционирования, так как нарушает независимость компонента

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,9 +4,7 @@ import styles from './Loader.module.scss';
 
 const Loader = () => {
   return (
-    <div className={styles.loaderWrapper}>
       <div className={styles.loader}></div>
-    </div>
   );
 };
 
