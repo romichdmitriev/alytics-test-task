a216f23491bfb7f31d78c924020685dc0bb14d11
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-07-05 21:03:45 +0500

    feat: добавляю thunk creator

---
+++ src/redux/thunk/getTableData.js
@@ -0,0 +1,13 @@
+import { fetchTableDataFailure, fetchTableDataStart, fetchTableDataSuccess } from '../actions/tableDataActions';
+import { tableData } from '../../tableData/tableData';
+
+export const getTableData = () => {
+  return dispatch => {
+    try {
+      dispatch(fetchTableDataStart());
+      dispatch(fetchTableDataSuccess(tableData));
+    } catch (error) {
+      dispatch(fetchTableDataFailure(error));
+    }
+  };
+};
